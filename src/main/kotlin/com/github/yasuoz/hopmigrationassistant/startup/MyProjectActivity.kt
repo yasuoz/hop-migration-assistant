124e39842cbe8e43d95b6a2720d8834a8b5b129c
GitHub Action <action@github.com>
2026-07-11 07:08:11 +0000

    Template cleanup

--- src/main/kotlin/com/github/yasuoz/hopmigrationassistant/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/yasuoz/hopmigrationassistant/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.yasuoz.hopmigrationassistant.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
--- src/main/kotlin/com/github/yasuoz/hopmigrationassistant/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/yasuoz/hopmigrationassistant/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.yasuoz.hopmigrationassistant.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.yasuoz.hopmigrationassistant.MyBundle
+import com.github.yasuoz.hopmigrationassistant.services.MyProjectService
 import javax.swing.JButton
 
 
